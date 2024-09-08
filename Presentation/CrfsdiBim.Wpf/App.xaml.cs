60ad1621da44c5d3c5c5943a05f381f200c0b8fe
zhengmaoch <zhengmaoch@msn.com>
2024-09-08 15:53:22 +0800

    update

--- Presentation/CrfsdiBim.Wpf/App.xaml.cs
+++ Presentation/CrfsdiBim.Wpf/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using CrfsdiBim.Wpf.ViewModels;
+using CrfsdiBim.Wpf.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System;
+using System.Windows;
+using CrfsdiBim.Services;
+using Serilog;
 
 namespace CrfsdiBim.Wpf
 {
@@ -7,5 +14,38 @@ namespace CrfsdiBim.Wpf
     /// </summary>
     public partial class App : Application
     {
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ILogger>(_ =>
+            {
+                return new LoggerConfiguration().MinimumLevel
+                .Debug()
+                    .WriteTo.File("log.txt")
+                    .CreateLogger();
+            });
+            services.AddSingleton<IRouteService, RouteService>();
+            services.AddSingleton<ITunnelService, TunnelService>();
+            services.AddTransient<MainWindowViewModel>();
+            services.AddTransient<MainWindow>(sp => new MainWindow { DataContext = sp.GetService<MainWindowViewModel>() });
+
+            return services.BuildServiceProvider();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = Services.GetService<MainWindow>();
+            mainWindow!.Show();
+        }
     }
 }
